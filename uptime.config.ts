94ed9df5b812b2645e721cc45c712e1b33e88ce2
paieer <paieer@users.noreply.github.com>
2024-09-28 15:54:11 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,8 @@
 const pageConfig = {
-  // Title for your status page
-  title: "Cloud Status Page",
-  // Links shown at the header of your status page, could set `highlight` to `true`
+  title: "Clouds Status Page",
   links: [
     { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'mailto:paieer@gmail.com', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -14,27 +11,45 @@ const workerConfig = {
   passwordProtection: 'username:password',
   monitors: [
     {
-      id: 'foo_monitor',
-      name: 'My API Monitor',
+      id: 'google_monitor',
+      name: 'My Google Monitor',
       method: 'GET',
       target: 'https://www.google.com'
     },
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
+      id: 'ssh_monitor',
+      name: 'Example SSH Monitor',
       method: 'TCP_PING',
       target: '1.2.3.4:22'
     },
-    // You can continue to define more monitors here...
   ],
-  notification: {
-    //...
-  },
   callbacks: {
-    //...
+    onStatusChange: async (
+      env: any,
+      monitor: any,
+      isUp: boolean,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string,
+    ) => {
+      // This callback will be called when there's a status change for any monitor
+      // Write any Typescript code here
+
+      // This will not follow the grace period settings and will be called immediately when the status changes
+      // You need to handle the grace period manually if you want to implement it
+    },
+    onIncident: async (
+      env: any,
+      monitor: any,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string,
+    ) => {
+      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
+      // Write any Typescript code here
+    },
   },
 }
-
 
 // Don't forget this, otherwise compilation fails.
 export { pageConfig, workerConfig }
